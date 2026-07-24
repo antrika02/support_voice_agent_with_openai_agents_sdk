5432b00528efffd6597fb2cd677875d9c72a5b64
Antrika Kashyap <antrikakashyap2@gmail.com>
2026-07-24 23:53:41 +0530

    Streamlit nterface created

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Customer Support Voice Agent",
+    page_icon="🎙️",
+    layout="wide"
+)
+
+st.title("🎙️ Customer Support Voice Agent")
+
+st.write("Hello Antrika 👋")
+
+st.success("Phase 1 setup completed successfully!")
